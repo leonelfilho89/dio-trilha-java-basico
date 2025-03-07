d71f1680630f803c4bd74add2945e3bc8feecbe8
Leonel <leoneljf@algartelecom.com.br>
2025-03-07 01:11:45 -0300

    Desenvolvimento desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita e lê os dados do usuário
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha
+        
+        System.out.println("Por favor, digite seu nome:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite seu saldo:");
+        double saldo = scanner.nextDouble();
+        
+        // Exibe a mensagem final
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                          "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
+        
+        scanner.close();
+
     }
 }
